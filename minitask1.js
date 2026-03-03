c5bf98b9a0b704ebb609582bbe02a13b6a63918c
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-03-03 15:57:39 +0700

    Update minitask1.js

--- minitask1.js
+++ minitask1.js
@@ -1,24 +1,12 @@
-import moment from "moment"
-import readline from "node:readline"
-
-const rd = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+import moment from 'moment';
 
 const dateConversion = (dateInput) => {
-    const mmt = moment(dateInput, "DD-MM-YYYY", true)
-    if (mmt.isValid()) {
-        return mmt.format("DD/MM/YYYY")
-    } else {
-        return "Format tanggal salah"
-    }
-}
-rd.question("Masukkan tanggal (DD-MM-YYYY): ", (answer) => {
-    const result = dateConversion(answer);
-    console.log(`Hasil DD/MM/YYYY: ${result}`);
-    
-    rd.close();
-});
+  const mmt = moment(dateInput, 'DD-MM-YYYY', true);
+  if (mmt.isValid()) {
+    return mmt.format('DD/MM/YYYY');
+  } else {
+    return 'Format tanggal salah';
+  }
+};
 
 export default dateConversion;
